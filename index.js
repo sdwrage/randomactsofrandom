d09a454476213e0223923e5ca002657908f292c7
Justin Lonas <sdwrage@gmail.com>
2013-09-12 02:44:29 -0400

    initial push

---
+++ index.js
@@ -0,0 +1,25 @@
+var request = require('request'),
+    jsdom = require('jsdom');
+    var url = process.argv[2]
+
+request({ uri: url }, function (error, response, body) {
+  if (error && response.statusCode !== 200) {
+    console.log('Error when contacting the given reddit page.... you did give a reddit post as a URL right?... RIGHT?!?!?!... okay i\'ll calm down...');
+  }
+  
+  jsdom.env({
+    html: body,
+    scripts: [
+      'http://code.jquery.com/jquery-1.5.min.js'
+    ], 
+    done: function (err, window) {
+      var $ = window.jQuery;
+      var comments = $(".entry");
+      var winner = comments[Math.floor(Math.random()*comments.length)]; // fetch the random winner
+      var winner_name = $(winner).find(".author").text();
+      var winner_comment = $(winner).find("")
+       // jQuery is now loaded on the jsdom window created from 'agent.body'
+       console.log("And the winner is.... " + winner_name + "!");
+    }
+  });
+});
